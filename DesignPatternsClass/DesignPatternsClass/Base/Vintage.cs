7581a5e62ec5dfa55e22948e05010c96f88a5916
Jacob Shaw <JacobShawCode@gmail.com>
2018-09-18 21:09:37 -0500

    Base Files for Design Patterns

---
+++ DesignPatternsClass/DesignPatternsClass/Base/Vintage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsClass
+{
+    public class Vintage : AbstractRoadBike
+    {
+        public Vintage()
+            : this(BikeColor.Red)
+        {
+
+        }
+
+        public Vintage(BikeColor color)
+            : base(color)
+        {
+
+        }
+    }
+}
---
+++ DesignPatternsClass/DesignPatternsClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Singleton;
+
+namespace DesignPatternsClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingeltonPatternDemo();
+        }
+
+        private static void SingeltonPatternDemo()
+        {
+            SerialNumberGenerator generator = SerialNumberGenerator.Instance;
+
+            Console.WriteLine("next serial: " + generator.NextSerial);
+            Console.WriteLine("next serial: " + generator.NextSerial);
+            Console.WriteLine("next serial: " + generator.NextSerial);
+            Console.WriteLine("next serial: " + generator.NextSerial);
+        }
+
+    }
+}
